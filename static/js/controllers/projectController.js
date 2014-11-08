b3e0fdd1e1a2babb04479ba298d2a0c1a20eea30
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2014-11-08 17:45:45 +0200

    Project Template

---
+++ static/js/controllers/projectController.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+app.controller('projectCtrl', ['$scope', 'loginService', '$location', 'adminService', '$rootScope',
+    function ($scope, loginService, $routeParams, adminService, $rootScope, $window, $location) {
+        $scope.txt = 'Page Home';
+        $scope.logout = function () {
+            loginService.logout();
+        }
+        loginService.TeamID;
+        $scope.TeamID_root = loginService.TeamID;
+        console.log();
+
+        console.log($scope.TeamID_root);
+        adminService.getMembers($scope.TeamID_root, $rootScope);
+        adminService.getProject($scope.TeamID_root, $rootScope);
+    }]);
---
+++ static/js/directives/projectDirective.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+app.directive('project', function () {
+    return {
+        templateUrl: 'static/template/project_template.html'
+    }
+});
