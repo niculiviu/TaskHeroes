a9b0519431778f8eac74f95aba9a3f5cf8dce48d
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2014-11-16 19:24:08 +0200

    index controller
    Am facut niste corectari, acum apare modalul de register de pe /login

---
+++ static/js/controllers/indexController.js
@@ -0,0 +1,7 @@
+﻿'use strict';
+
+app.controller('indexCtrl', ['$scope', 'loginService', '$location', 'adminService', '$rootScope', 'ngDialog',
+    function ($scope, loginService, $routeParams, adminService, $rootScope, ngDialog) {
+        $scope.txt = 'Page Home';
+        $rootScope.PageName = 'Dashboard';
+    }]);
