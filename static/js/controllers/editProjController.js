fe91bdee2e1fb8fd4dab3722d7cd0548112b70a5
liviunicu <liviu-alexandru.nicu@my.fmi.unibuc.ro>
2014-11-25 22:07:15 +0200

    Edit project interface

---
+++ static/js/controllers/editProjController.js
@@ -0,0 +1,22 @@
+﻿app.controller('editprojectCtrl', ['$scope', 'loginService', '$location', 'projectService', '$rootScope', 'adminService', 'ngDialog',
+    function ($scope, loginService, $routeParams, projectService, $rootScope, adminService, ngDialog) {
+        $scope.txt = 'Page Home';
+        $rootScope.PageName = 'Edit Project';
+        $scope.logout = function () {
+            loginService.logout();
+        }
+        loginService.TeamID;
+        $scope.TeamID_root = loginService.TeamID;
+        $rootScope.permision = true;
+        console.log($scope.TeamID_root);
+       
+        $scope.projectID = $rootScope.ProjectID;
+
+        $scope.param = $routeParams.$$url;
+        var url = $scope.param.split('/');
+        console.log(url[3]);
+        projectService.getSingleProject(url[3], $scope.TeamID_root, $rootScope, $scope);
+        adminService.getMembers($scope.TeamID_root, $rootScope);
+        adminService.getProject($scope.TeamID_root, $rootScope);
+        projectService.getLists(url[3], $rootScope);
+    }]);
